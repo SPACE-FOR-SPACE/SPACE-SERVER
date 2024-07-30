835ff34f1783d2ba2da9f2479893dee1f371f4af
GSB0203 <gimseungbin0203@gmail.com>
2024-07-30 17:19:33 +0900

    feat(Item): "Item Entity"
    Item Entity 및 Category Enum 생성

---
+++ src/main/java/com/space/server/core/domain/item/Item.java
@@ -0,0 +1,41 @@
+package com.space.server.core.domain.item;
+
+import com.space.server.core.domain.item.value.Category;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "item")
+@Entity
+public class Item {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "item_id")
+  @NotNull
+  private Long id;
+
+  @NotNull
+  private String name;
+
+  @NotNull
+  private int price;
+
+  @Column(name = "item_image")
+  private String image;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private Category category;
+
+  @Builder
+  public Item(String name, int price, String image, Category category) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+    this.category = category;
+  }
+}
+
---
+++ src/main/java/com/space/server/core/domain/item/value/Category.java
@@ -0,0 +1,6 @@
+package com.space.server.core.domain.item.value;
+
+public enum Category {
+  HEAD,
+  THEME
+}
