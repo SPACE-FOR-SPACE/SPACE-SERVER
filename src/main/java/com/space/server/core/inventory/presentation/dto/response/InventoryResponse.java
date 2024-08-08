b55b3aedc33b0557b731f0aa60d5f611d87174c4
GSB0203 <gimseungbin0203@gmail.com>
2024-08-09 01:17:40 +0900

    fix(inventory): 인벤토리 조회 오류 해결
    인벤토리 엔티티를 Serialize 할 때 LAZY 설정으로 비어있는 객체를 Serialize 하려 할 때 생기는 오류 해결

--- src/main/java/com/space/server/core/inventory/presentation/dto/response/InventoryResponse.java
+++ src/main/java/com/space/server/core/inventory/presentation/dto/response/InventoryResponse.java
@@ -1,16 +1,19 @@
 package com.space.server.core.inventory.presentation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.space.server.core.inventory.domain.Inventory;
 import com.space.server.core.item.domain.Item;
 
 public record InventoryResponse(
     Long id,
     //User user,
+    @JsonIgnore
     Item head,
+    @JsonIgnore
     Item theme,
     int point
 ) {
-  public static InventoryResponse from(Inventory equip) {
-    return new InventoryResponse(equip.getId(), equip.getHead(), equip.getTheme(), equip.getPoint());
+  public static InventoryResponse from(Inventory inventory) {
+    return new InventoryResponse(inventory.getId(), inventory.getHead(), inventory.getTheme(), inventory.getPoint());
   }
 }
