bb2c978010d18311a15c48c81bc2bbb58cec2c1b
YunChan-Oh <oyunchan9@gmail.com>
2024-08-13 23:49:36 +0900

    feat(user): UserEntity 구현

---
+++ src/main/java/com/space/server/user/domain/User.java
@@ -0,0 +1,69 @@
+package com.space.server.user.domain;
+
+
+import com.space.server.user.domain.value.Role;
+import com.space.server.user.domain.value.SocialType;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType; // KAKAO, NAVER, GOOGLE
+
+    private String socialId; // 로그인한 소셜 타입의 식별자 값 (일반 로그인인 경우 null)
+
+    private String refreshToken; // 리프레시 토큰
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Builder
+    public User(String email, String password, String nickname, int age) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.age = age;
+    }
+
+    // 유저 권한 설정 메소드
+    public void authorizeUser() {
+        this.role = Role.USER;
+    }
+
+    // 비밀번호 암호화 메소드
+    public void passwordEncode(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(this.password);
+    }
+
+    public void updateRefreshToken(String updateRefreshToken) {
+        this.refreshToken = updateRefreshToken;
+    }
+}
