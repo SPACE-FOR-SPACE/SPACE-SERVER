382bb2e4920315261cf4ad4de505cd2394281b57
GSB0203 <gimseungbin0203@gmail.com>
2024-09-19 02:27:30 +0900

    refactor(quiz): 퀴즈 불푤요한 import 제거
    퀴즈 내에 불피요한 import를 제거하였습니다.

--- src/main/java/com/space/server/core/quiz/service/implementation/QuizCreator.java
+++ src/main/java/com/space/server/core/quiz/service/implementation/QuizCreator.java
@@ -1,6 +1,5 @@
 package com.space.server.core.quiz.service.implementation;
 
-import com.space.server.core.chapter.domain.Chapter;
 import com.space.server.core.quiz.domain.Quiz;
 import com.space.server.core.quiz.domain.repository.QuizRepository;
 import lombok.RequiredArgsConstructor;
