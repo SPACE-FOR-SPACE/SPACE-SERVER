d78ac75f5479b54c37f8e8e54436419d26ce2178
GSB0203 <gimseungbin0203@gmail.com>
2024-09-19 02:15:31 +0900

    refactor(checklist): 체크리스트 반환 수정
    체크리스트를 반환할 때 구분하기 힘들어 id를 추가하였습니다.

--- src/main/java/com/space/server/core/checklist/presentation/dto/response/ChecklistResponse.java
+++ src/main/java/com/space/server/core/checklist/presentation/dto/response/ChecklistResponse.java
@@ -5,12 +5,14 @@ import com.space.server.core.checklist.domain.Checklist;
 import com.space.server.core.quiz.domain.Quiz;
 
 public record ChecklistResponse(
+    Long checklistId,
     Quiz quiz,
     Chapter chapter,
     String content
 ) {
   public static ChecklistResponse from(Checklist checklist) {
     return new ChecklistResponse(
+        checklist.getId(),
         checklist.getQuiz(),
         checklist.getChapter(),
         checklist.getContent()
