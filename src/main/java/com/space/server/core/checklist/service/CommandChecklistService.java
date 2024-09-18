0b1d538c6cb4215455b780abc0b82ee45627ae20
GSB0203 <gimseungbin0203@gmail.com>
2024-09-19 01:56:31 +0900

    feat(checklist): 체크리스트 업데이트 기능 개발
    체크리스트 업데이트를 구현하였습니다.

--- src/main/java/com/space/server/core/checklist/service/CommandChecklistService.java
+++ src/main/java/com/space/server/core/checklist/service/CommandChecklistService.java
@@ -5,6 +5,8 @@ import com.space.server.core.chapter.service.implementation.ChapterReader;
 import com.space.server.core.checklist.domain.Checklist;
 import com.space.server.core.checklist.presentation.dto.request.ChecklistRequest;
 import com.space.server.core.checklist.service.implementation.ChecklistCreator;
+import com.space.server.core.checklist.service.implementation.ChecklistReader;
+import com.space.server.core.checklist.service.implementation.ChecklistUpdater;
 import com.space.server.core.quiz.domain.Quiz;
 import com.space.server.core.quiz.service.implementation.QuizReader;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class CommandChecklistService {
 
   private final ChecklistCreator checklistCreator;
+  private final ChecklistReader checklistReader;
+  private final ChecklistUpdater checklistUpdater;
   private final QuizReader quizReader;
   private final ChapterReader chapterReader;
 
@@ -25,5 +29,14 @@ public class CommandChecklistService {
     Chapter chapter = chapterReader.findById(request.chapterId());
     Checklist checklist = new Checklist(quiz, chapter, request.content());
     checklistCreator.create(checklist);
+  }
+
+  public void updateChecklist(Long checklistId, ChecklistRequest request) {
+    Checklist checklist = checklistReader.readOne(checklistId);
+    Quiz quiz = quizReader.findById(request.quizId());
+    Chapter chapter = chapterReader.findById(request.chapterId());
+    Checklist updateableChecklist = new Checklist(quiz, chapter, request.content());
+    checklistUpdater.update(updateableChecklist, checklist);
+
   }
 }
