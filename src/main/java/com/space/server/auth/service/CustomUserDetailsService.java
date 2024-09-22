7c438c0075c531d96c61d4ef1cdb3c3233f73c5f
YunChan-Oh <oyunchan9@gmail.com>
2024-09-23 05:52:21 +0900

    feat(auth): CustomUserDetailsService 구현

---
+++ src/main/java/com/space/server/auth/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.space.server.auth.service;
+
+import com.space.server.auth.service.dto.CustomUserDetails;
+import com.space.server.user.domain.Users;
+import com.space.server.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Users user = userRepository.findByEmail(email);
+
+        if (user != null) {
+
+            return new CustomUserDetails(user);
+        }
+
+        return null;
+    }
+}
