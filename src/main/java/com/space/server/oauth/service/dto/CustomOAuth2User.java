4a535f82027702b0062298e177437766b29be9eb
YunChan-Oh <oyunchan9@gmail.com>
2024-09-26 08:29:08 +0900

    fix(auth): Principal 위한 getName 반환 값 지정

--- src/main/java/com/space/server/oauth/service/dto/CustomOAuth2User.java
+++ src/main/java/com/space/server/oauth/service/dto/CustomOAuth2User.java
@@ -30,7 +30,7 @@ public class CustomOAuth2User implements OAuth2User {
 
     @Override
     public String getName() {
-        return null;
+        return userDto.email();
     }
 
     public String getEmail() {
