67a7f844b5d7972968cc1ba01c600bb4b008450b
GSB0203 <gimseungbin0203@gmail.com>
2024-09-26 17:54:11 +0900

    refactor(inventory&item): 맴핑 연결 수정
    맵핑 시 api를 넣어야 했었는데 불필요하다가 판단하여 제거했습니다.

--- src/main/java/com/space/server/core/inventory/presentation/InventoryController.java
+++ src/main/java/com/space/server/core/inventory/presentation/InventoryController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/inventories")
+@RequestMapping("/inventories")
 public class InventoryController {
 
   private final CommandInventoryService commandInventoryService;
