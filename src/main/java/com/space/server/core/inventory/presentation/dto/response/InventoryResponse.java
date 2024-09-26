530ce871f5104a09bc24485ea2fe8dd5b38ee105
GSB0203 <gimseungbin0203@gmail.com>
2024-09-26 17:52:57 +0900

    feat(item): 인벤토리 내의 아이템 조회 로직 수정
    인벤토리 내의 있는 아이템을 조회할 때 카테고리도 반환하게 수정하였습니다.

--- src/main/java/com/space/server/core/inventory/presentation/dto/response/InventoryResponse.java
+++ src/main/java/com/space/server/core/inventory/presentation/dto/response/InventoryResponse.java
@@ -1,18 +1,21 @@
 package com.space.server.core.inventory.presentation.dto.response;
 
 import com.space.server.core.inventory.domain.Inventory;
+import com.space.server.core.item.domain.value.Category;
 import lombok.Builder;
 
 @Builder
 public record InventoryResponse(
     Long id,
     Long itemId,
+    Category category,
     boolean isEquipped
 ) {
   public static InventoryResponse from(Inventory inventory) {
     return InventoryResponse.builder()
         .id(inventory.getId())
         .itemId(inventory.getItem().getId())
+        .category(inventory.getItem().getCategory())
         .isEquipped(inventory.isEquipped())
         .build();
   }
