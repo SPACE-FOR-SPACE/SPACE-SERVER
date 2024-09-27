a60ebebb49435c8afafd1b206c77b81f57f0ca69
YunChan-Oh <oyunchan9@gmail.com>
2024-09-28 07:08:33 +0900

    refactor(auth): JWT sub 값 email에서 id로 변경

--- src/main/java/com/space/server/oauth/handler/CustomSuccessHandler.java
+++ src/main/java/com/space/server/oauth/handler/CustomSuccessHandler.java
@@ -7,6 +7,7 @@ import com.space.server.oauth.service.dto.CustomOAuth2User;
 import com.space.server.user.domain.Users;
 import com.space.server.user.domain.repository.UserRepository;
 import com.space.server.user.domain.value.Role;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,16 +34,16 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         log.warn("오어스 성공 핸들러" + customUserDetails.toString());
 
-        String email = customUserDetails.getEmail();
+        Long id = customUserDetails.getId();
 
         Role role = customUserDetails.getAuthorities().stream()
                 .findFirst()
                 .map(a -> Role.fromValue(a.getAuthority()))
                 .orElse(Role.GUEST);
 
-        Users user = userRepository.findByEmail(email);
+        Users user = userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
         boolean isNewUser = user == null || user.getAge() == null;
-
+        log.warn("오어스 성공 핸들러 중간 체크 : "+isNewUser);
         if (isNewUser) {
             role = Role.GUEST;
             if (user != null) {
@@ -52,17 +53,20 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
 
-        String accessToken = jwtUtil.createAccessToken(email, role);
-        String refreshToken = jwtUtil.createRefreshToken(email, role);
+        String accessToken = jwtUtil.createAccessToken(id, role);
+        String refreshToken = jwtUtil.createRefreshToken(id, role);
 
-        jwtUtil.addRefreshToken(email, refreshToken);
+        jwtUtil.addRefreshToken(id, refreshToken);
 
         response.addCookie(jwtUtil.createAccessCookie("access_social", accessToken));
         response.addCookie(jwtUtil.createRefreshCookie("refresh_social", refreshToken));
 
         if (isNewUser) {
-            response.sendRedirect("http://localhost:3000/additional-info");
+            log.warn("오어스 성공 핸들러 새로운 유저");
+            response.sendRedirect("http://localhost:3000/");
+//            response.sendRedirect("http://localhost:3000/additional-info");
         } else {
+            log.warn("오어스 성공 핸들러 원래 있던 유저");
             response.sendRedirect("http://localhost:3000/");
         }
     }
