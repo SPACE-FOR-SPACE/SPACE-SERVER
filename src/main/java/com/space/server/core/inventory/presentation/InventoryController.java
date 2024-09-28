e6df99e748d38979e7ae4cba6b6d94948994225f
GSB0203 <gimseungbin0203@gmail.com>
2024-09-28 15:41:42 +0900

    feat(inventory): 인벤토리 swagger 추가
    인벤토리 entity, dto, controller의 api 설명을 위해 swagger를 이용하여 설명을 추가였습니다.

--- src/main/java/com/space/server/core/inventory/presentation/InventoryController.java
+++ src/main/java/com/space/server/core/inventory/presentation/InventoryController.java
@@ -4,6 +4,8 @@ import com.space.server.core.inventory.presentation.dto.request.InventoryRequest
 import com.space.server.core.inventory.presentation.dto.response.InventoryResponse;
 import com.space.server.core.inventory.service.CommandInventoryService;
 import com.space.server.core.inventory.service.QueryInventoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -19,16 +21,21 @@ public class InventoryController {
   private final QueryInventoryService queryInventoryService;
 
   @PostMapping("/inventory")
-  public void createInventory(@RequestBody InventoryRequest request) {
+  @Operation(summary = "인벤토리 생성", description = "인벤토리를 생성합니다.")
+  public void createInventory(
+      @Parameter(description = "아이템 ID", required = true) @RequestBody InventoryRequest request) {
     commandInventoryService.createInventory(request.itemId(), SecurityContextHolder.getContext().getAuthentication());
   }
 
   @GetMapping("/{inventory-id}")
-  public InventoryResponse readOne(@PathVariable("inventory-id") Long inventoryId) {
+  @Operation(summary = "인벤토리 조회", description = "해당 인벤토리를 조회합니다.")
+  public InventoryResponse readOne(
+      @Parameter(description = "조회할 인벤토리 ID", required = true) @PathVariable("inventory-id") Long inventoryId) {
     return InventoryResponse.from(queryInventoryService.readOne(inventoryId));
   }
 
   @GetMapping
+  @Operation(summary = "전체 인벤토리 조회", description = "전체 인벤토리를 조회합니다.")
   public List<InventoryResponse> readAll() {
     return queryInventoryService.readMine(SecurityContextHolder.getContext().getAuthentication()).stream()
         .map(InventoryResponse::from)
@@ -36,19 +43,24 @@ public class InventoryController {
   }
 
   @GetMapping("/equip")
+  @Operation(summary = "장착 인벤토리 조회", description = "아이템이 장착된 인벤토리를 조회합니다.")
   public List<InventoryResponse> readIsEquipped() {
     return queryInventoryService.readIsEquipped(SecurityContextHolder.getContext().getAuthentication()).stream()
-        .map((InventoryResponse::from))
+        .map(InventoryResponse::from)
         .toList();
   }
 
   @PutMapping("/{inventory-id}")
-  public void equipItem(@PathVariable(name = "inventory-id") Long inventoryId) {
+  @Operation(summary = "아이템 장착", description = "해당 인벤토리의 아이템을 장착합니다.")
+  public void equipItem(
+      @Parameter(description = "장착할 인벤토리 ID", required = true) @PathVariable(name = "inventory-id") Long inventoryId) {
     commandInventoryService.equipInventory(inventoryId, SecurityContextHolder.getContext().getAuthentication());
   }
 
   @DeleteMapping("/{inventory-id}")
-  public void deleteInventory(@PathVariable(name = "inventory-id") Long inventoryId) {
+  @Operation(summary = "인벤토리 삭제", description = "해당 인벤토리를 삭제합니다.")
+  public void deleteInventory(
+      @Parameter(description = "삭제할 인벤토리 ID", required = true) @PathVariable(name = "inventory-id") Long inventoryId) {
     commandInventoryService.deleteInventory(inventoryId);
   }
 }
