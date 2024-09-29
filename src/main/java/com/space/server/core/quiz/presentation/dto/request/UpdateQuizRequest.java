4c5ca29f7b931b6139edcbb3ea59a98e324ae565
GSB0203 <gimseungbin0203@gmail.com>
2024-09-30 04:14:36 +0900

    feat(quiz): 스웨거 예시 추가
    dto에 예시를 추가하였습니다.

--- src/main/java/com/space/server/core/quiz/presentation/dto/request/UpdateQuizRequest.java
+++ src/main/java/com/space/server/core/quiz/presentation/dto/request/UpdateQuizRequest.java
@@ -6,10 +6,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UpdateQuizRequest(
 
-    @Schema(description = "맵 정보", required = true)
+    @Schema(description = "맵 정보", example = "7x7 2차원 배열", required = true)
     Integer[][] map,
 
-    @Schema(description = "캐릭터 방향", required = true)
+    @Schema(description = "캐릭터 방향", example = "RIGHT", required = true)
     CharacterDirection characterDirection
 ) {
   public Quiz toEntity() {
