6b57727883586e9fee2dd355715afbae494b95c3
GSB0203 <gimseungbin0203@gmail.com>
2024-09-30 04:24:20 +0900

    feat(chapter): 스웨거 추가
    entity, controller, dto, parameter 예시를 추가하였습니다.

--- src/main/java/com/space/server/core/chapter/domain/Chapter.java
+++ src/main/java/com/space/server/core/chapter/domain/Chapter.java
@@ -1,6 +1,7 @@
 package com.space.server.core.chapter.domain;
 
 import io.hypersistence.utils.hibernate.type.basic.PostgreSQLHStoreType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -14,23 +15,28 @@ import java.util.Map;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Schema(description = "챕터 Entity")
 public class Chapter {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(description = "챕터 ID")
   private Long id;
 
   @NotNull
+  @Schema(description = "챕터 설명")
   private String explanation;
 
   @Type(PostgreSQLHStoreType.class)
   @Column(columnDefinition = "hstore")
   @NotNull
+  @Schema(description = "맵오브젝트")
   private Map<String, String> mapObject;
 
   @Type(PostgreSQLHStoreType.class)
   @Column(columnDefinition = "hstore")
   @NotNull
+  @Schema(description = "맵오브젝트 이미지")
   private Map<String, String> mapObjectImage;
 
   @Builder
--- src/main/java/com/space/server/core/chapter/presentation/dto/request/ChapterRequest.java
+++ src/main/java/com/space/server/core/chapter/presentation/dto/request/ChapterRequest.java
@@ -1,16 +1,25 @@
 package com.space.server.core.chapter.presentation.dto.request;
 
 import com.space.server.core.chapter.domain.Chapter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Map;
 
 public record ChapterRequest(
+    @Schema(description = "챕터 설명", example = "바다와 관련된 챕터입니다.")
     String explanation,
-    Map<String, String> mapObject,
-    Map<String, String> mapObjectImage
 
+    @Schema(description = "맵오브젝트", example = "{\"1\": \"chapter\", \"2\": \"target\"}")
+    Map<String, String> mapObject,
+
+    @Schema(description = "맵오브젝트 이미지", example = "{\"1\": \"chapterURL\", \"2\": \"targetURL\"}")
+    Map<String, String> mapObjectImage
 ) {
   public Chapter toEntity() {
-    return new Chapter(explanation, mapObject, mapObjectImage);
+    return Chapter.builder()
+        .explanation(explanation)
+        .mapObject(mapObject)
+        .mapObjectImage(mapObjectImage)
+        .build();
   }
 }
