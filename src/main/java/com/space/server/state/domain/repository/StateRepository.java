23f6592fa96fc49fe0e87c3e9e399346260f1d03
YunChan-Oh <oyunchan9@gmail.com>
2024-09-30 15:09:27 +0900

    feat(state): StateRepository 구현

---
+++ src/main/java/com/space/server/state/domain/repository/StateRepository.java
@@ -0,0 +1,14 @@
+package com.space.server.state.domain.repository;
+
+import com.space.server.core.quiz.domain.Quiz;
+import com.space.server.state.domain.State;
+import com.space.server.user.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Long> {
+    Optional<State> findByQuizAndUser(Quiz quiz, Users user);
+}
