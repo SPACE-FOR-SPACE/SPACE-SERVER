f4bc003b35784e66d2d0ae1ccdbbaadc42e923a2
YunChan-Oh <oyunchan9@gmail.com>
2024-09-30 15:06:25 +0900

    feat(chat): ChatController 구현

---
+++ src/main/java/com/space/server/chat/presentation/ChatController.java
@@ -0,0 +1,27 @@
+package com.space.server.chat.presentation;
+
+import com.space.server.chat.presentation.dto.request.CreateChatRequest;
+import com.space.server.chat.service.CommandChatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.space.server.common.jwt.util.AuthenticationUtil.getMemberId;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final CommandChatService commandChatService;
+//    private final QueryChatService queryChatService;
+
+    @PostMapping("/chats/{quiz-id}")
+    public void createChat(
+            @PathVariable("quiz-id") Long quizId,
+            @RequestBody CreateChatRequest request
+            ) {
+        commandChatService.create(quizId, request, getMemberId());
+    }
+}
