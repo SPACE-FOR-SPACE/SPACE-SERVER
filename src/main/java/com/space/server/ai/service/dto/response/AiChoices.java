02a5a3e0fdba031b6f341ad5956caa258541058b
YunChan-Oh <oyunchan9@gmail.com>
2024-09-30 17:31:04 +0900

    feat(ai): AiChoices 구현

---
+++ src/main/java/com/space/server/ai/service/dto/response/AiChoices.java
@@ -0,0 +1,14 @@
+package com.space.server.ai.service.dto.response;
+
+import com.space.server.ai.service.dto.request.AiChat;
+
+import java.util.List;
+import java.util.Map;
+
+public record AiChoices(
+    Integer index,
+    List<AiChat> message,
+    String logprobs,
+    String finish_reason
+) {
+}
