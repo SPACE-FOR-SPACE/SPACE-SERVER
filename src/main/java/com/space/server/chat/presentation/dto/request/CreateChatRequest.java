d7f629bb45ce354edfad6fde8db4450d5f0ee60d
YunChan-Oh <oyunchan9@gmail.com>
2024-09-30 15:07:14 +0900

    feat(chat): CreateChatRequest 구현

---
+++ src/main/java/com/space/server/chat/presentation/dto/request/CreateChatRequest.java
@@ -0,0 +1,11 @@
+package com.space.server.chat.presentation.dto.request;
+
+import com.space.server.chat.domain.Chat;
+
+public record CreateChatRequest(
+        String userChat
+) {
+//    public Chat toEntity() {
+//
+//    }
+}
