ecc7d851dc846d2d77e0068b73bbe58089ac600d
YunChan-Oh <oyunchan9@gmail.com>
2024-10-02 08:26:07 +0900

    feat(ai): 상태에 대한 모든 챗 반환

--- src/main/java/com/space/server/chat/service/implementation/ChatReader.java
+++ src/main/java/com/space/server/chat/service/implementation/ChatReader.java
@@ -1,9 +1,12 @@
 package com.space.server.chat.service.implementation;
 
+import com.space.server.chat.domain.Chat;
 import com.space.server.chat.domain.repository.ChatRepository;
 import com.space.server.state.domain.State;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,4 +17,6 @@ public class ChatReader {
     public Integer findMaxOrderByState(State state) {
         return chatRepository.findMaxOrderByState(state);
     }
+
+    public List<Chat> findAllChatByStateId(State state) { return chatRepository.findAllChatByStateId(state); }
 }
