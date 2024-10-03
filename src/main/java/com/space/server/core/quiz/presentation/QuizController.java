c39eb99ce764eb157c7ed7a8f5e120b0253b6c62
GSB0203 <gimseungbin0203@gmail.com>
2024-10-04 01:23:39 +0900

    fix(quiz): quiz 불필요 api 제거
    서비스에는 불필요한 생성, 수정, 삭제 api를 삭제하였습니다.

--- src/main/java/com/space/server/core/quiz/presentation/QuizController.java
+++ src/main/java/com/space/server/core/quiz/presentation/QuizController.java
@@ -1,9 +1,6 @@
 package com.space.server.core.quiz.presentation;
 
-import com.space.server.core.quiz.presentation.dto.request.QuizRequest;
-import com.space.server.core.quiz.presentation.dto.request.UpdateQuizRequest;
 import com.space.server.core.quiz.presentation.dto.response.QuizResponse;
-import com.space.server.core.quiz.service.CommandQuizService;
 import com.space.server.core.quiz.service.QueryQuizService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -14,28 +11,13 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/chapters")
+
 public class QuizController {
 
-  private final CommandQuizService commandQuizService;
   private final QueryQuizService queryQuizService;
 
-  @PostMapping("/quizzes/quiz")
-  @Operation(summary = "퀴즈 생성", description = "퀴즈를 생성합니다.")
-  public void createQuiz(@RequestBody QuizRequest request) {
-    commandQuizService.create(request);
-  }
-
-  @GetMapping("/chapters/{chapter-id}/steps/{step-id}")
-  @Operation(summary = "챕터Id 및 스텝Id로 퀴즈 조회", description = "해당 챕터Id와 스텝Id의 퀴즈를 조회합니다.")
-  public QuizResponse findOne(
-      @Parameter(description = "챕터 ID", required = true) @PathVariable("chapter-id") Long chapterId,
-      @Parameter(description = "스텝 ID", required = true) @PathVariable("step-id") Long stepId
-  ) {
-    return QuizResponse.from(queryQuizService.findOne(chapterId, stepId));
-  }
-
-  @GetMapping("/chapters/{chapter-id}/steps")
+  @GetMapping("/{chapter-id}/steps")
   @Operation(summary = "챕터 내 모든 퀴즈 조회", description = "해당 챕터의 모든 퀴즈를 조회합니다.")
   public List<QuizResponse> findAll(
       @Parameter(description = "챕터 ID", required = true) @PathVariable("chapter-id") Long chapterId
@@ -45,36 +27,12 @@ public class QuizController {
         .toList();
   }
 
-  @GetMapping("/quizzes/{quiz-id}")
-  @Operation(summary = "퀴즈Id로 조회", description = "해당 퀴즈를 조회합니다.")
-  public QuizResponse readOne(
-      @Parameter(description = "퀴즈 ID", required = true) @PathVariable("quiz-id") Long quizId
+  @GetMapping("/{chapter-id}/steps/{step-id}")
+  @Operation(summary = "챕터Id 및 스텝Id로 퀴즈 조회", description = "해당 챕터의 해당 퀴즈를 조회합니다.")
+  public QuizResponse findOne(
+      @Parameter(description = "챕터 ID", required = true) @PathVariable("chapter-id") Long chapterId,
+      @Parameter(description = "스텝 ID", required = true) @PathVariable("step-id") Long stepId
   ) {
-    return QuizResponse.from(queryQuizService.readOne(quizId));
-  }
-
-  @GetMapping("/quizzes")
-  @Operation(summary = "모든 퀴즈 조회", description = "전체 퀴즈를 조회합니다.")
-  public List<QuizResponse> readAll() {
-    return queryQuizService.readAll().stream()
-        .map(QuizResponse::from)
-        .toList();
-  }
-
-  @PutMapping("/quizzes/{quiz-id}")
-  @Operation(summary = "퀴즈 업데이트", description = "해당 퀴즈를 업데이트합니다.")
-  public void updateQuiz(
-      @Parameter(description = "퀴즈 ID", required = true) @PathVariable("quiz-id") Long quizId,
-      @RequestBody UpdateQuizRequest request
-  ) {
-    commandQuizService.update(quizId, request.toEntity());
-  }
-
-  @DeleteMapping("/quizzes/{quiz-id}")
-  @Operation(summary = "퀴즈 삭제", description = "해당 퀴즈를 삭제합니다.")
-  public void deleteQuiz(
-      @Parameter(description = "퀴즈 ID", required = true) @PathVariable("quiz-id") Long quizId
-  ) {
-    commandQuizService.delete(quizId);
+    return QuizResponse.from(queryQuizService.findOne(chapterId, stepId));
   }
 }
