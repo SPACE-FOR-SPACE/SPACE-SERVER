3038a6842dc16368f06f100c6427bd27aa95ddba
GSB0203 <gimseungbin0203@gmail.com>
2024-10-04 01:22:35 +0900

    fix(chapter): chapter 불필요 api 제거
    서비스에는 불필요한 생성, 수정, 삭제 api를 삭제하였습니다.

--- src/main/java/com/space/server/core/chapter/presentation/ChapterController.java
+++ src/main/java/com/space/server/core/chapter/presentation/ChapterController.java
@@ -1,12 +1,9 @@
 package com.space.server.core.chapter.presentation;
 
-import com.space.server.core.chapter.presentation.dto.request.ChapterRequest;
 import com.space.server.core.chapter.presentation.dto.response.ChapterResponse;
-import com.space.server.core.chapter.service.CommandChapterService;
 import com.space.server.core.chapter.service.QueryChapterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,17 +11,10 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/chapters")
+@RequestMapping("/chapters")
 public class ChapterController {
 
-  private final CommandChapterService commandChapterService;
   private final QueryChapterService queryChapterService;
-
-  @PostMapping("/chapter")
-  @Operation(summary = "챕터 생성", description = "챕터를 생성합니다.")
-  public void createChapter(@RequestBody ChapterRequest request) {
-    commandChapterService.createChapter(request.toEntity());
-  }
 
   @GetMapping("{chapter-id}")
   @Operation(summary = "챕터 조회", description = "해당 챕터를 조회합니다.")
@@ -39,19 +29,5 @@ public class ChapterController {
     return queryChapterService.readAll().stream()
         .map(ChapterResponse::from)
         .toList();
-  }
-
-  @PutMapping("/{chapter-id}")
-  @Operation(summary = "챕터 업데이트", description = "해당 챕터를 업데이트합니다.")
-  public void updateChapter(
-      @Parameter(description = "챕터 ID", required = true) @PathVariable(name = "chapter-id") Long chapterId,
-      @RequestBody ChapterRequest request) {
-    commandChapterService.updateChapter(chapterId, request.toEntity());
-  }
-
-  @DeleteMapping("/{chapter-id}")
-  @Operation(summary = "챕터 삭제", description = "해당 챕터를 삭제합니다.")
-  public void deleteChapter(@Parameter(description = "챕터 ID", required = true) @PathVariable(name = "chapter-id") Long chapterId) {
-    commandChapterService.deleteChapter(chapterId);
   }
 }
