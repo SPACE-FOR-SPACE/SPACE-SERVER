3eac6840f91ad2e135f4be130371822ec411076f
GSB0203 <gimseungbin0203@gmail.com>
2024-10-04 11:55:10 +0900

    fix(updater): 불필요한 코드 제거
    생성자 주입이 필요 없지만 생성자 주입 어노테이션이 붙여져 있어 제거하였습니다.

--- src/main/java/com/space/server/core/chapter/service/implementation/ChapterUpdater.java
+++ src/main/java/com/space/server/core/chapter/service/implementation/ChapterUpdater.java
@@ -1,11 +1,9 @@
 package com.space.server.core.chapter.service.implementation;
 
 import com.space.server.core.chapter.domain.Chapter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class ChapterUpdater {
 
   public void update(Chapter updatableChapter, Chapter chapter) {
--- src/main/java/com/space/server/core/quiz/service/implementation/QuizUpdater.java
+++ src/main/java/com/space/server/core/quiz/service/implementation/QuizUpdater.java
@@ -1,11 +1,9 @@
 package com.space.server.core.quiz.service.implementation;
 
 import com.space.server.core.quiz.domain.Quiz;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class QuizUpdater {
 
   public void update(Quiz updatableQuiz, Quiz quiz) {
