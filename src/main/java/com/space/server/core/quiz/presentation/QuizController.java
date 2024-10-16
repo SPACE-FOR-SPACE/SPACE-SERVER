de124e2c07ccd300878fa33faa02d4065cf48750
GSB0203 <gimseungbin0203@gmail.com>
2024-10-17 01:14:16 +0900

    remove(quiz): Quiz 챕터 및 스텝 아이디 조회 제거
    필요없는 조회 로직을 제거하였습니다.

--- src/main/java/com/space/server/core/quiz/presentation/QuizController.java
+++ src/main/java/com/space/server/core/quiz/presentation/QuizController.java
@@ -26,13 +26,4 @@ public class QuizController {
         .map(QuizResponse::from)
         .toList();
   }
-
-  @GetMapping("/{chapter-id}/steps/{step-id}")
-  @Operation(summary = "챕터Id 및 스텝Id로 퀴즈 조회", description = "해당 챕터의 해당 퀴즈를 조회합니다.")
-  public QuizResponse findOne(
-      @Parameter(description = "챕터 ID", required = true) @PathVariable("chapter-id") Long chapterId,
-      @Parameter(description = "스텝 ID", required = true) @PathVariable("step-id") Long stepId
-  ) {
-    return QuizResponse.from(queryQuizService.findOne(chapterId, stepId));
-  }
 }
