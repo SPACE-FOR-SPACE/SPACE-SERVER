9cc196da5a8da5399a81cd2824392e740d4e6b27
이재현 <2023050@bssm.hs.kr>
2024-10-18 21:35:12 +0900

    fix(AI): model state의 map 관련 코드 삭제

--- src/main/java/com/space/server/state/presentation/dto/response/StateResponse.java
+++ src/main/java/com/space/server/state/presentation/dto/response/StateResponse.java
@@ -10,7 +10,6 @@ public record StateResponse(
         Users userId,
         Quiz quizId,
         Status status,
-        Integer[][] map,
         Long[] score
 ) {
     public static StateResponse from(State state) {
@@ -19,7 +18,6 @@ public record StateResponse(
                 state.getUser(),
                 state.getQuiz(),
                 state.getStatus(),
-                state.getMap(),
                 state.getScore()
         );
     }
