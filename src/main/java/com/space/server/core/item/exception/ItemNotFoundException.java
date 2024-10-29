81743f18ae8f51725424d5e5b92ae4e64bdfbb5e
GSB0203 <gimseungbin0203@gmail.com>
2024-10-29 16:10:51 +0900

    feat(exception): ItemNotFoundException
    예외를 세부적으로 나누었습니다.

---
+++ src/main/java/com/space/server/core/item/exception/ItemNotFoundException.java
@@ -0,0 +1,11 @@
+package com.space.server.core.item.exception;
+
+import com.space.server.common.exception.SpaceException;
+import org.springframework.http.HttpStatus;
+
+public class ItemNotFoundException extends SpaceException {
+
+  public ItemNotFoundException() {
+    super(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다.");
+  }
+}
