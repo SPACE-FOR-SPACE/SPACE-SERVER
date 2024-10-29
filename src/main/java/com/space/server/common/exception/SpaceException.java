ac53bfea8eecd91c6ea99e6e099a44406e9f8725
GSB0203 <gimseungbin0203@gmail.com>
2024-10-29 14:38:24 +0900

    refactor(exception): SpaceException
    예외를 더 세부적으로 처리하기 위해 수정하였습니다.

--- src/main/java/com/space/server/common/exception/SpaceException.java
+++ src/main/java/com/space/server/common/exception/SpaceException.java
@@ -2,10 +2,12 @@ package com.space.server.common.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public class SpaceException extends RuntimeException {
-    private final ErrorCode errorCode;
+    private final HttpStatus status;
+    private final String message;
 }
 
