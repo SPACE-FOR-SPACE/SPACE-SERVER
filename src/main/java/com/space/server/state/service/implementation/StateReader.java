3d09855bc3ebbcf44abbcf943a7df86dc027abb6
GSB0203 <gimseungbin0203@gmail.com>
2024-10-29 16:16:03 +0900

    refactor(exception): StateReader
    세부적으로 나눈 예외로 변경하였습니다.

--- src/main/java/com/space/server/state/service/implementation/StateReader.java
+++ src/main/java/com/space/server/state/service/implementation/StateReader.java
@@ -1,10 +1,9 @@
 package com.space.server.state.service.implementation;
 
-import com.space.server.common.exception.ErrorCode;
-import com.space.server.common.exception.SpaceException;
 import com.space.server.core.quiz.domain.Quiz;
 import com.space.server.state.domain.State;
 import com.space.server.state.domain.repository.StateRepository;
+import com.space.server.state.exception.StateNotFoundException;
 import com.space.server.user.domain.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +18,7 @@ public class StateReader {
 
     public State findById(Long stateId) {
         return stateRepository.findById(stateId)
-                .orElseThrow(() -> new SpaceException(ErrorCode.STATE_NOT_FOUND));
+                .orElseThrow(StateNotFoundException::new);
     }
 
     public Optional<State> findByQuizIdAndUserId(Quiz quiz, Users user) {
