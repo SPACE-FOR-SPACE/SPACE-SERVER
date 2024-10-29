e0df1d59b521411fe725a702d72f2e21c571ab84
GSB0203 <gimseungbin0203@gmail.com>
2024-10-29 15:02:46 +0900

    remove(exception): ErrorCode
    사용하지 않게 된 코드를 삭제하였습니다.

--- src/main/java/com/space/server/common/exception/ErrorCode.java
+++ src/main/java/com/space/server/common/exception/ErrorCode.java
@@ -7,13 +7,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    // auth
-    EMPTY_AUTHORIZATION_HEADER(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
-    SECRET_KEY_MISMATCH(HttpStatus.UNAUTHORIZED, "기존의 시크릿 키와 일치하지 않습니다."),
-    AUTH_REQUIRED(HttpStatus.UNAUTHORIZED, "인증 정보가 필요합니다."),
-
     // user
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
     INSUFFICIENT_POINTS(HttpStatus.FORBIDDEN, "포인트가 부족합니다."),
@@ -25,17 +18,12 @@ public enum ErrorCode {
 
     // item
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다."),
-    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 조회할 수 없습니다."),
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
 
     // inventory
-    INVENTORY_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "아이템을 가지고 있지 않습니다."),
     INVENTORY_ITEM_FOUND(HttpStatus.FOUND, "아이템을 이미 가지고 있습니다."),
-    ITEM_ALREADY_EQUIPPED(HttpStatus.CONFLICT, "아이템이 이미 장착되었습니다."),
 
     // quiz
     QUIZ_NOT_FOUND(HttpStatus.NOT_FOUND, "문제를 찾을 수 없습니다."),
-    STEP_NOT_FOUND(HttpStatus.NOT_FOUND, "단게를 찾을 수 없습니다."),
 
     //state
     STATE_NOT_FOUND(HttpStatus.NOT_FOUND, "상태를 찾을 수 없습니다."),
