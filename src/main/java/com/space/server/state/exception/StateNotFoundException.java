f9037f7e30f9da642b913c95de2d86230820ef34
GSB0203 <gimseungbin0203@gmail.com>
2024-10-29 16:10:06 +0900

    feat(exception): StateNotFoundException
    예외를 세부적으로 나누었습니다.

---
+++ src/main/java/com/space/server/state/exception/StateNotFoundException.java
@@ -0,0 +1,11 @@
+package com.space.server.state.exception;
+
+import com.space.server.common.exception.SpaceException;
+import org.springframework.http.HttpStatus;
+
+public class StateNotFoundException extends SpaceException {
+
+  public StateNotFoundException() {
+    super(HttpStatus.NOT_FOUND, "상태를 찾을 수 없습니다.");
+  }
+}
