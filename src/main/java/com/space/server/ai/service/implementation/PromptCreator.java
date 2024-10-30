d572379ed131bf0742490e069e0dcca70857c04a
이재현 <2023050@bssm.hs.kr>
2024-10-30 14:24:33 +0900

    fix(AI): 프롬프트 개선

--- src/main/java/com/space/server/ai/service/implementation/PromptCreator.java
+++ src/main/java/com/space/server/ai/service/implementation/PromptCreator.java
@@ -26,12 +26,13 @@ public class PromptCreator {
         return "상태는 '"+ type +"', " +
             "문제 난이도는 '0', " +
             "문제 내용은 '"+ quiz.getContent() +"', " +
-            "문제 조건들은 '"+ checklist +"', " +
+            "문제 조건들은 '"+ checklist +"', 문제 조건 중에서 하나라도 맞으면 정답이야." +
             "맵은 '"+ Arrays.deepToString(quiz.getMap()) +"', 맵은 무조건 7*7 2차원 배열이야." +
             "맵의 오브젝트는 '" + totalMapObject + "', " +
             "캐릭터 방향은 '"+ quiz.getCharacterDirection() +"', " +
             "입력은 '" + chat + "'" +
             "일 때 정답인지 아닌지와 문제 조건들 중에서 어떤 것들이 맞는지 리스트로 나타내고," +
-            "입력에 대한 피드백과 움직임과 움직임을 나타낸 맵을 JSON 형태로 만들어줘.";
+            "입력에 대한 피드백과 움직임과 움직임을 나타낸 맵을 JSON 형태로 만들어. " +
+            "근데 결과 말고는 json으로 만들지 마. 그리고 응답이 왜 그런지도 설명해.";
     }
 }
