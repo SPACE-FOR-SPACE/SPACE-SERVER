ad1254827c21d1f6891f4dbd66895cec68d2f0fd
YunChan-Oh <oyunchan9@gmail.com>
2024-11-04 08:35:30 +0900

    refactor(exception): ItemNotFoundException
    에러 코드를 추가했습니다.

--- src/main/java/com/space/server/core/item/exception/ItemNotFoundException.java
+++ src/main/java/com/space/server/core/item/exception/ItemNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class ItemNotFoundException extends SpaceException {
 
   public ItemNotFoundException() {
-    super(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다.");
+    super(HttpStatus.NOT_FOUND, "ITEM_NOT_FOUND", "아이템을 찾을 수 없습니다.");
   }
 }
