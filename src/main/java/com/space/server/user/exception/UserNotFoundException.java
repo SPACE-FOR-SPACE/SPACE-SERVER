34e00fe9b79dd4f9e5a22cb930896c6dce55bdd2
YunChan-Oh <oyunchan9@gmail.com>
2024-11-04 08:37:02 +0900

    refactor(exception): UserNotFoundException
    에러 코드를 추가했습니다.

--- src/main/java/com/space/server/user/exception/UserNotFoundException.java
+++ src/main/java/com/space/server/user/exception/UserNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class UserNotFoundException extends SpaceException {
 
   public UserNotFoundException() {
-    super(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
+    super(HttpStatus.NOT_FOUND, "USER_NOT_FOUND","유저를 찾을 수 없습니다.");
   }
 }
