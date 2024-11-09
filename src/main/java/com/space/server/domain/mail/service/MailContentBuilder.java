7e3c784183132d3becac8dc569caad42a8b47482
YunChan-Oh <oyunchan9@gmail.com>
2024-11-09 16:29:02 +0900

    feat(mail): MailContentBuilder
    메일 템플릿 생성 클래스를 구현했습니다.

---
+++ src/main/java/com/space/server/domain/mail/service/MailContentBuilder.java
@@ -0,0 +1,18 @@
+package com.space.server.domain.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+@RequiredArgsConstructor
+public class MailContentBuilder {
+    private final TemplateEngine templateEngine;
+
+    public String build(String message) {
+        Context context = new Context();
+        context.setVariable("link", message);
+        return templateEngine.process("mailTemplate", context);
+    }
+}
