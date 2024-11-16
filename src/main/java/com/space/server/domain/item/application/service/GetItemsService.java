5dfc6e9e4f1c99ea3cbc6aac2f1c7ac5140ebd5b
GSB0203 <gimseungbin0203@gmail.com>
2024-11-16 17:52:48 +0900

    refactor(Item): GetItemsService
    아이템 전체 조회를 위한 단일 책임 적용 및 클린 아키텍처 적용 Service

---
+++ src/main/java/com/space/server/domain/item/application/service/GetItemsService.java
@@ -0,0 +1,21 @@
+package com.space.server.domain.item.application.service;
+
+import com.space.server.domain.common.UseCase;
+import com.space.server.domain.item.application.port.in.GetItemsQuery;
+import com.space.server.domain.item.application.port.out.LoadItemsPort;
+import com.space.server.domain.item.domain.Item;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@UseCase
+@RequiredArgsConstructor
+public class GetItemsService implements GetItemsQuery {
+
+  private final LoadItemsPort loadItemsPort;
+
+  @Override
+  public List<Item> getItems() {
+    return loadItemsPort.loadItems();
+  }
+}
