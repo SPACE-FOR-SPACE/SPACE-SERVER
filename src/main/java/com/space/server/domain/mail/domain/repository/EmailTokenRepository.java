1e6eca77fa440321ba246dbba5da5ce4235142d5
YunChan-Oh <oyunchan9@gmail.com>
2024-11-16 21:35:03 +0900

    feat(mail): EmailTokenRepository
    이메일 토큰 레포지토리를 구현했습니다.

---
+++ src/main/java/com/space/server/domain/mail/domain/repository/EmailTokenRepository.java
@@ -0,0 +1,15 @@
+package com.space.server.domain.mail.domain.repository;
+
+import com.space.server.domain.mail.domain.EmailToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface EmailTokenRepository extends JpaRepository<EmailToken, Long> {
+    EmailToken findByToken(String token);
+    EmailToken findByEmailAndIsVerifiedFalse(String email);
+    EmailToken findByEmailAndIsVerified(String email, boolean verified);
+    void deleteByExpiryDateBefore(LocalDateTime now);
+}
