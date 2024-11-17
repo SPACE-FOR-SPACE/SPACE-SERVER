49f2e546db63e942ba30f58777c7e06bc50de044
GSB0203 <gimseungbin0203@gmail.com>
2024-11-17 23:48:13 +0900

    refactor(Inventory): CheckHaveItemAdapter
    아이템 이미 구매했는지 확인을 위한 단일 책임 적용 및 클린 아키텍처 적용 Adapter

---
+++ src/main/java/com/space/server/domain/inventory/adapter/out/persistence/CheckHaveItemAdapter.java
@@ -0,0 +1,23 @@
+package com.space.server.domain.inventory.adapter.out.persistence;
+
+import com.space.server.domain.common.PersistenceAdapter;
+import com.space.server.domain.inventory.application.port.out.CheckHaveItemPort;
+import com.space.server.domain.item.adapter.out.persistence.ItemJpaEntity;
+import com.space.server.domain.item.adapter.out.persistence.ItemMapper;
+import com.space.server.domain.item.domain.Item;
+import com.space.server.domain.user.domain.Users;
+import lombok.RequiredArgsConstructor;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class CheckHaveItemAdapter implements CheckHaveItemPort {
+
+  private final InventoryRepository inventoryRepository;
+  private final ItemMapper itemMapper;
+
+  @Override
+  public Boolean checkHaveItem(Item item, Users user) {
+    ItemJpaEntity itemJpaEntity = itemMapper.mapToItemJpaEntity(item);
+    return inventoryRepository.existsByItemAndUser(itemJpaEntity, user);
+  }
+}
