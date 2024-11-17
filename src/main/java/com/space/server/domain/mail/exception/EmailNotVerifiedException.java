5529b3f98d1be7b05bc98ca9763ebc78db199dec
YunChan-Oh <oyunchan9@gmail.com>
2024-11-17 17:32:25 +0900

    refactor(mail): EmailNotVerifiedException
    메일 관련 예외들을 분리해서 처리하도록 수정했습니다.

--- src/main/java/com/space/server/domain/mail/exception/EmailNotVerifiedException.java
+++ src/main/java/com/space/server/domain/mail/exception/EmailNotVerifiedException.java
@@ -1,9 +1,9 @@
 package com.space.server.domain.mail.exception;
 
-import com.space.server.common.exception.SpaceException;
+import com.space.server.common.exception.mail.SpaceMailException;
 import org.springframework.http.HttpStatus;
 
-public class EmailNotVerifiedException extends SpaceException {
+public class EmailNotVerifiedException extends SpaceMailException {
     public EmailNotVerifiedException() {
         super(HttpStatus.BAD_REQUEST, "EMAIL_NOT_VERIFIED", "이메일 인증이 필요합니다.");
     }
