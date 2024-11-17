595794a6eb03782d05ca284a2e5c9ec899459410
YunChan-Oh <oyunchan9@gmail.com>
2024-11-17 17:32:39 +0900

    refactor(mail): EmailSendFailedException
    메일 관련 예외들을 분리해서 처리하도록 수정했습니다.

--- src/main/java/com/space/server/domain/mail/exception/EmailSendFailedException.java
+++ src/main/java/com/space/server/domain/mail/exception/EmailSendFailedException.java
@@ -1,9 +1,9 @@
 package com.space.server.domain.mail.exception;
 
-import com.space.server.common.exception.SpaceException;
+import com.space.server.common.exception.mail.SpaceMailException;
 import org.springframework.http.HttpStatus;
 
-public class EmailSendFailedException extends SpaceException {
+public class EmailSendFailedException extends SpaceMailException {
   public EmailSendFailedException() {
     super(HttpStatus.INTERNAL_SERVER_ERROR, "EMAIL_SEND_FAILED", "이메일 전송에 실패했습니다.");
   }
