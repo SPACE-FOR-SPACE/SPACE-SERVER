ef6a8c9cfa0dd4f60decfceaa18d28ee5abcad70
GSB0203 <gimseungbin0203@gmail.com>
2024-11-17 22:14:23 +0900

    refactor(Inventory): CreateInventoryAdapter
    인벤토리 생성을 위한 단일 책임 적용 및 클린 아키텍처 적용 Adapter

---
+++ src/main/java/com/space/server/domain/inventory/adapter/out/persistence/CreateInventoryAdapter.java
@@ -0,0 +1,24 @@
+package com.space.server.domain.inventory.adapter.out.persistence;
+
+import com.space.server.domain.common.PersistenceAdapter;
+import com.space.server.domain.inventory.application.port.out.CreateInventoryPort;
+import com.space.server.domain.inventory.domain.Inventory;
+import com.space.server.domain.item.domain.Item;
+import com.space.server.domain.user.domain.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@PersistenceAdapter
+@RequiredArgsConstructor
+public class CreateInventoryAdapter implements CreateInventoryPort {
+
+  @Override
+  public Inventory create(Item item, Users user) {
+    return Inventory.builder()
+        .item(item)
+        .user(user)
+        .isEquipped(false)
+        .build();
+  };
+}
