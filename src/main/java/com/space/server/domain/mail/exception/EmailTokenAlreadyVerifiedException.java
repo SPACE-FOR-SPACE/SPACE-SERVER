e4f179db03ffa9bf62b0ec903af75835edce5297
YunChan-Oh <oyunchan9@gmail.com>
2024-11-17 17:32:53 +0900

    refactor(mail): EmailTokenAlreadyVerifiedException
    메일 관련 예외들을 분리해서 처리하도록 수정했습니다.

--- src/main/java/com/space/server/domain/mail/exception/EmailTokenAlreadyVerifiedException.java
+++ src/main/java/com/space/server/domain/mail/exception/EmailTokenAlreadyVerifiedException.java
@@ -1,9 +1,9 @@
 package com.space.server.domain.mail.exception;
 
-import com.space.server.common.exception.SpaceException;
+import com.space.server.common.exception.mail.SpaceMailException;
 import org.springframework.http.HttpStatus;
 
-public class EmailTokenAlreadyVerifiedException extends SpaceException {
+public class EmailTokenAlreadyVerifiedException extends SpaceMailException {
     public EmailTokenAlreadyVerifiedException() {
         super(HttpStatus.BAD_REQUEST, "EMAIL_TOKEN_ALREADY_VERIFIED", "이미 인증된 토큰입니다.");
     }
