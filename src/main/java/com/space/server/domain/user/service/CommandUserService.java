6213e668241c7a9b3216cd932539af3429aa12d5
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 16:56:29 +0900

    feat(user): CommandUserService
    유저 관련 기능을 구현했습니다.

--- src/main/java/com/space/server/domain/user/service/CommandUserService.java
+++ src/main/java/com/space/server/domain/user/service/CommandUserService.java
@@ -1,5 +1,11 @@
 package com.space.server.domain.user.service;
 
+import com.space.server.domain.inventory.service.implementation.InventoryDeleter;
+import com.space.server.domain.state.service.implementation.StateDeleter;
+import com.space.server.domain.user.domain.Users;
+import com.space.server.domain.user.service.implementation.UserDeleter;
+import com.space.server.domain.user.service.implementation.UserReader;
+import com.space.server.domain.user.service.implementation.UserUpdater;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,4 +15,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommandUserService {
 
+    private final UserUpdater  userUpdater;
+    private final UserReader userReader;
+    private final UserDeleter userDeleter;
+    private final StateDeleter stateDeleter;
+    private final InventoryDeleter inventoryDeleter;
+
+    public void updateUser(Long userId, Users user) {
+        Users updatableUser = userReader.findById(userId);
+
+        userUpdater.update(updatableUser, user);
+    }
+
+    public void deleteUser(Long userId) {
+        Users deletableUser = userReader.findById(userId);
+
+        inventoryDeleter.deleteByUser(deletableUser);
+        stateDeleter.deleteByUser(deletableUser);
+        userDeleter.delete(deletableUser);
+    }
 }
