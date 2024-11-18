335e43bd5ac444d88afadef276fd76e05169873e
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 17:19:44 +0900

    feat(user): StateDeleter
    유저 삭제 중 참조 무결성을 위해서 상태 삭제를 구현했습니다.

--- src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
+++ src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
@@ -2,6 +2,7 @@ package com.space.server.domain.state.service.implementation;
 
 import com.space.server.domain.state.domain.State;
 import com.space.server.domain.state.domain.repository.StateRepository;
+import com.space.server.domain.user.domain.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,9 @@ public class StateDeleter {
 
     public void delete(State state) {
         stateRepository.delete(state);
+    }
+
+    public void deleteByUser(Users user) {
+        stateRepository.deleteByUser(user);
     }
 }
