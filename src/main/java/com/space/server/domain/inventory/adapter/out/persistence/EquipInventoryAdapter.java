9e2a88f3a0f09e6655a7d52cc027e63806d5e0b7
GSB0203 <gimseungbin0203@gmail.com>
2024-11-19 01:38:00 +0900

    remove(Inventory): EquipInventoryAdapter
    다중 책임 분리

--- src/main/java/com/space/server/domain/inventory/adapter/out/persistence/EquipInventoryAdapter.java
+++ src/main/java/com/space/server/domain/inventory/adapter/out/persistence/EquipInventoryAdapter.java
@@ -3,10 +3,6 @@ package com.space.server.domain.inventory.adapter.out.persistence;
 import com.space.server.common.annotation.PersistenceAdapter;
 import com.space.server.domain.inventory.application.port.out.EquipInventoryPort;
 import com.space.server.domain.inventory.domain.Inventory;
-import com.space.server.domain.item.domain.value.Category;
-import com.space.server.domain.user.domain.Users;
-import com.space.server.domain.user.domain.repository.UserRepository;
-import com.space.server.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,16 +13,7 @@ public class EquipInventoryAdapter implements EquipInventoryPort {
 
   private final InventoryRepository inventoryRepository;
   private final InventoryMapper inventoryMapper;
-  private final UserRepository userRepository;
 
-  @Override
-  public Inventory findEquippedInventoryByCategoryAndUser(Category category, Long userId) {
-    Users user = userRepository.findById(userId)
-        .orElseThrow(UserNotFoundException::new);
-
-    InventoryJpaEntity equippedByCategoryInventory = inventoryRepository.findByCategoryAndUserAndIsEquipped(category, user);
-    return inventoryMapper.mapToInventory(equippedByCategoryInventory);
-  }
   @Override
   public void equipInventory(Inventory inventory) {
     inventory.equip();
