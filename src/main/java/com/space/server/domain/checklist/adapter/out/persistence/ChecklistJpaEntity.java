ad4d1274cc704fc17f9500cd0fccce25a34a238a
GSB0203 <gimseungbin0203@gmail.com>
2024-11-18 19:27:53 +0900

    refactor(Checklist): ChecklistJpaEntity
    Domain Entity와 Jpa Entity 분리

---
+++ src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistJpaEntity.java
@@ -0,0 +1,33 @@
+package com.space.server.domain.checklist.adapter.out.persistence;
+
+import com.space.server.domain.chapter.domain.Chapter;
+import com.space.server.domain.quiz.domain.Quiz;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@Table(name = "checklist")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChecklistJpaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @JoinColumn
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Quiz quiz;
+
+  @JoinColumn
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Chapter chapter;
+
+  @Column
+  private String content;
+}
