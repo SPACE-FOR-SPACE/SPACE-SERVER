0a24d520a260b90dcaa979028969095a176832d9
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 17:20:30 +0900

    feat(user): UserController
    유저 컨트롤러 구현했습니다.

--- src/main/java/com/space/server/domain/user/presentation/UserController.java
+++ src/main/java/com/space/server/domain/user/presentation/UserController.java
@@ -1,24 +1,42 @@
 package com.space.server.domain.user.presentation;
 
 
+import com.space.server.domain.user.presentation.dto.request.UserRequest;
 import com.space.server.domain.user.presentation.dto.response.UserResponse;
 import com.space.server.domain.user.service.CommandUserService;
 import com.space.server.domain.user.service.QueryUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.space.server.common.jwt.util.AuthenticationUtil.getMemberId;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
+@Tag(name = "User", description = "유저 API")
 public class UserController {
 
     private final CommandUserService commandUserService;
     private final QueryUserService queryUserService;
 
-    @GetMapping("/users")
+    @GetMapping
+    @Operation(summary = "유저 조회", description = "유저의 정보를 조회합니다.")
     public UserResponse readOne() {
         return UserResponse.from(queryUserService.readOne(getMemberId()));
     }
+
+    @PutMapping
+    @Operation(summary = "유저 수정", description = "유저의 정보를 수정합니다.")
+    public void update(@RequestBody UserRequest userRequest) {
+        commandUserService.updateUser(getMemberId(), userRequest.toEntity());
+    }
+
+    @DeleteMapping
+    @Operation(summary = "유저 삭제", description = "유저의 정보를 삭제합니다.")
+    public void delete() {
+        commandUserService.deleteUser(getMemberId());
+    }
+
 }
