49bdb70963f5a88471e96137508b4d76ef384e62
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 19:20:18 +0900

    fix(user): StateDeleter
    유저 삭제 중 참조 무결성을 위해서 채팅 삭제를 구현했습니다.

--- src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
+++ src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
@@ -1,5 +1,6 @@
 package com.space.server.domain.state.service.implementation;
 
+import com.space.server.domain.chat.service.implementation.ChatDeleter;
 import com.space.server.domain.state.domain.State;
 import com.space.server.domain.state.domain.repository.StateRepository;
 import com.space.server.domain.user.domain.Users;
@@ -11,12 +12,16 @@ import org.springframework.stereotype.Service;
 public class StateDeleter {
 
     private final StateRepository stateRepository;
+    private final ChatDeleter chatDeleter;
+    private final StateReader stateReader;
 
     public void delete(State state) {
+        chatDeleter.deleteByState(state);
         stateRepository.delete(state);
     }
 
     public void deleteByUser(Users user) {
-        stateRepository.deleteByUser(user);
+        State deletableState = stateReader.findByUser(user);
+        delete(deletableState);
     }
 }
