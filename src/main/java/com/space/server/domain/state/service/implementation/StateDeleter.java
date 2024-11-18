e496b0a90cb0c6b57b4dd6da17b51bf1d6e21ad9
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 19:50:03 +0900

    fix(user): StateDeleter
    State가 없는 유저도 삭제되도록 수정했습니다.

--- src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
+++ src/main/java/com/space/server/domain/state/service/implementation/StateDeleter.java
@@ -5,8 +5,10 @@ import com.space.server.domain.state.domain.State;
 import com.space.server.domain.state.domain.repository.StateRepository;
 import com.space.server.domain.user.domain.Users;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class StateDeleter {
@@ -21,7 +23,12 @@ public class StateDeleter {
     }
 
     public void deleteByUser(Users user) {
-        State deletableState = stateReader.findByUser(user);
-        delete(deletableState);
+        stateReader.findByUserId(user).ifPresentOrElse(
+                deletableState -> {
+                    log.warn("State 삭제: " + deletableState);
+                    delete(deletableState);
+                },
+                () -> log.warn("해당 유저에는 State가 없습니다.: " + user)
+        );
     }
 }
