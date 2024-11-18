78625a28916eb96cfa6036ee54d1b9ecc4f37ace
YunChan-Oh <oyunchan9@gmail.com>
2024-11-18 17:22:52 +0900

    refactor(user): UserResponse
    유저 객체 반환용 DTO를 구현했습니다.

--- src/main/java/com/space/server/domain/user/presentation/dto/response/UserResponse.java
+++ src/main/java/com/space/server/domain/user/presentation/dto/response/UserResponse.java
@@ -1,11 +1,25 @@
 package com.space.server.domain.user.presentation.dto.response;
 
 import com.space.server.domain.user.domain.Users;
+import com.space.server.domain.user.domain.value.Role;
 
 public record UserResponse(
-        Long userId
+        Long id,
+        String username,
+        String type,
+        String email,
+        Role role,
+        Integer age,
+        Integer point
 ) {
-    public static UserResponse from(Users users) {
-        return new UserResponse(users.getId());
+    public static UserResponse from(Users user) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getType(),
+                user.getEmail(),
+                user.getRole(),
+                user.getAge(),
+                user.getPoint());
     }
 }
