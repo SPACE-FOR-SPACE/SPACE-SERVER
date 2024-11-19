1c09adf1b98cf63c00946f59d9eb53f333921663
GSB0203 <gimseungbin0203@gmail.com>
2024-11-19 19:24:20 +0900

    refactor(Item): ChecklistMapper
    Chapter 구현하여 클린 아키텍처 버전으로 수정하였습니다.

--- src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistMapper.java
+++ src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistMapper.java
@@ -1,16 +1,22 @@
 package com.space.server.domain.checklist.adapter.out.persistence;
 
+import com.space.server.domain.chapter.adapter.out.persistence.ChapterMapper;
 import com.space.server.domain.checklist.domain.Checklist;
+import com.space.server.domain.item.adapter.out.persistence.ItemMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ChecklistMapper {
+
+  private final ChapterMapper chapterMapper;
 
   public Checklist mapToChecklist(ChecklistJpaEntity checklist) {
     return Checklist.builder()
         .id(new Checklist.ChecklistId(checklist.getId()))
         .quiz(checklist.getQuiz())
-        .chapter(checklist.getChapter())
+        .chapter(chapterMapper.mapToChapter(checklist.getChapter()))
         .content(checklist.getContent())
         .build();
   }
