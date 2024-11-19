c2ac8a7af680c8368e47ca539006274ed00fe73a
GSB0203 <gimseungbin0203@gmail.com>
2024-11-19 14:07:26 +0900

    refactor(Chapter): Chapter
    Domain Entity와 Jpa Entity 분리

---
+++ src/main/java/com/space/server/domain/chapter/adapter/out/persistence/ChapterJpaEntity.java
@@ -0,0 +1,39 @@
+package com.space.server.domain.chapter.adapter.out.persistence;
+
+import io.hypersistence.utils.hibernate.type.basic.PostgreSQLHStoreType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import java.util.Map;
+
+@Builder
+@Entity
+@Table(name = "chapter")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChapterJpaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column
+  private String explanation;
+
+  @Type(PostgreSQLHStoreType.class)
+  @Column(columnDefinition = "hstore")
+  @NotNull
+  private Map<String, String> mapObject;
+
+  @Type(PostgreSQLHStoreType.class)
+  @Column(columnDefinition = "hstore")
+  @NotNull
+  private Map<String, String> mapObjectImage;
+
+}
