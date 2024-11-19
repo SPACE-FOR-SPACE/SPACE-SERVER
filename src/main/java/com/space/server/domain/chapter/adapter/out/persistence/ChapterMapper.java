4c557dff700ab871c8be63c9dc995c605ed6369c
GSB0203 <gimseungbin0203@gmail.com>
2024-11-19 14:12:24 +0900

    refactor(Chapter): ChapterMapper
    Domain Entity와 Jpa Entity 간 변환

---
+++ src/main/java/com/space/server/domain/chapter/adapter/out/persistence/ChapterMapper.java
@@ -0,0 +1,17 @@
+package com.space.server.domain.chapter.adapter.out.persistence;
+
+import com.space.server.domain.chapter.domain.Chapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChapterMapper {
+
+  public Chapter mapToChapter(ChapterJpaEntity chapter) {
+    return Chapter.builder()
+        .id(new Chapter.ChapterId(chapter.getId()))
+        .explanation(chapter.getExplanation())
+        .mapObject(chapter.getMapObject())
+        .mapObjectImage(chapter.getMapObjectImage())
+        .build();
+  }
+}
