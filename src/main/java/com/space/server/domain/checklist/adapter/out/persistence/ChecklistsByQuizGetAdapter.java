7f2490cfdf831fe3bb83ecbcf99708f99326070c
GSB0203 <gimseungbin0203@gmail.com>
2024-11-20 19:31:34 +0900

    refactor(Checklist): ChecklistsByQuizGetAdapter
    클린 아키텍처로 변경된 Jpa Entity 퀴즈로 수정하였습니다.

--- src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistsByQuizGetAdapter.java
+++ src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistsByQuizGetAdapter.java
@@ -3,8 +3,9 @@ package com.space.server.domain.checklist.adapter.out.persistence;
 import com.space.server.common.annotation.PersistenceAdapter;
 import com.space.server.domain.checklist.application.port.out.LoadChecklistsByQuizPort;
 import com.space.server.domain.checklist.domain.Checklist;
-import com.space.server.domain.quiz.domain.Quiz;
-import com.space.server.domain.quiz.service.implementation.QuizReader;
+import com.space.server.domain.quiz.adapter.out.persistence.QuizJpaEntity;
+import com.space.server.domain.quiz.adapter.out.persistence.QuizRepository;
+import com.space.server.domain.quiz.exception.QuizNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -15,11 +16,13 @@ public class ChecklistsByQuizGetAdapter implements LoadChecklistsByQuizPort {
 
   private final ChecklistRepository checklistRepository;
   private final ChecklistMapper checklistMapper;
-  private final QuizReader quizReader;
+  private final QuizRepository quizRepository;
 
   @Override
   public List<Checklist> loadChecklistsByQuiz(Long quizId) {
-    Quiz quiz = quizReader.findById(quizId);
+    QuizJpaEntity quiz = quizRepository.findById(quizId)
+        .orElseThrow(QuizNotFoundException::new);
+
     return checklistRepository.findByQuiz(quiz).stream()
         .map(checklistMapper::mapToChecklist)
         .toList();
