c3f465407072b993feab1e18615bb029dfb918e4
GSB0203 <gimseungbin0203@gmail.com>
2024-11-20 19:30:54 +0900

    refactor(Checklist): ChecklistRepository
    클린 아키텍처로 변경된 Jpa Entity 퀴즈로 수정하였습니다.

--- src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistRepository.java
+++ src/main/java/com/space/server/domain/checklist/adapter/out/persistence/ChecklistRepository.java
@@ -1,6 +1,6 @@
 package com.space.server.domain.checklist.adapter.out.persistence;
 
-import com.space.server.domain.quiz.domain.Quiz;
+import com.space.server.domain.quiz.adapter.out.persistence.QuizJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ChecklistRepository extends JpaRepository<ChecklistJpaEntity, Long> {
-  List<ChecklistJpaEntity> findByQuiz(Quiz quiz);
+  List<ChecklistJpaEntity> findByQuiz(QuizJpaEntity quiz);
 }
