9b0b870678a87e258f732e7fa23371fe355c85ed
GSB0203 <gimseungbin0203@gmail.com>
2024-11-20 19:32:53 +0900

    refactor(Chat): QueryChatService
    클린 아키텍처가 적용된 클래스로 수정하였습니다.

--- src/main/java/com/space/server/domain/chat/service/QueryChatService.java
+++ src/main/java/com/space/server/domain/chat/service/QueryChatService.java
@@ -3,7 +3,7 @@ package com.space.server.domain.chat.service;
 import com.space.server.domain.chat.domain.Chat;
 import com.space.server.domain.chat.presentation.dto.response.ChatResponse;
 import com.space.server.domain.chat.service.implementation.ChatReader;
-import com.space.server.domain.quiz.service.implementation.QuizReader;
+import com.space.server.domain.quiz.application.port.in.GetQuizQuery;
 import com.space.server.domain.state.service.implementation.StateReader;
 import com.space.server.domain.user.service.implementation.UserReader;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +20,12 @@ public class QueryChatService {
 
     private final ChatReader chatReader;
     private final StateReader stateReader;
-    private final QuizReader quizReader;
+    private final GetQuizQuery getQuizQuery;
     private final UserReader userReader;
 
     public List<ChatResponse> readChats(Long quizId, Long userId){
         List<Chat> chatList = chatReader.findAllChatByState(stateReader.findByQuizIdAndUserId(
-                quizReader.findById(quizId),
+                getQuizQuery.getQuiz(quizId),
                 userReader.findById(userId)
         ).get());
         List<ChatResponse> chatResponseList = new ArrayList<>();
