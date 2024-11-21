5102e880743305654bad693786e2e5f94b57c242
YunChan-Oh <oyunchan9@gmail.com>
2024-11-21 10:27:00 +0900

    fix(auth): CommandUserService
    유저 삭제 시 토큰도 함께 삭제되도록 수정했습니다.

--- src/main/java/com/space/server/domain/user/service/CommandUserService.java
+++ src/main/java/com/space/server/domain/user/service/CommandUserService.java
@@ -1,11 +1,14 @@
 package com.space.server.domain.user.service;
 
+import com.space.server.common.jwt.util.JwtUtil;
 import com.space.server.domain.inventory.service.implementation.InventoryDeleter;
 import com.space.server.domain.state.service.implementation.StateDeleter;
 import com.space.server.domain.user.domain.Users;
 import com.space.server.domain.user.service.implementation.UserDeleter;
 import com.space.server.domain.user.service.implementation.UserReader;
 import com.space.server.domain.user.service.implementation.UserUpdater;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,7 @@ public class CommandUserService {
     private final UserDeleter userDeleter;
     private final StateDeleter stateDeleter;
     private final InventoryDeleter inventoryDeleter;
+    private final JwtUtil jwtUtil;
 
     public void updateUser(Long userId, Users user) {
         Users updatableUser = userReader.findById(userId);
@@ -27,8 +31,12 @@ public class CommandUserService {
         userUpdater.update(updatableUser, user);
     }
 
-    public void deleteUser(Long userId) {
+    public void deleteUser(HttpServletRequest request, HttpServletResponse response, Long userId) {
         Users deletableUser = userReader.findById(userId);
+
+        String refreshToken = jwtUtil.getTokenFromCookies(request, "refresh_normal", "refresh_social");
+
+        jwtUtil.deleteAllTokens(response, refreshToken);
 
         inventoryDeleter.deleteByUser(deletableUser);
         stateDeleter.deleteByUser(deletableUser);
