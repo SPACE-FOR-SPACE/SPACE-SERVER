f49723c662da029840867349a04a6e1548d442a6
GSB0203 <gimseungbin0203@gmail.com>
2024-11-23 18:45:44 +0900

    refactor(Chat): ReadQuizAndUserRequest
    컨트롤러 및 dto의 매개변수 순서를 동일화하였습니다.

--- src/main/java/com/space/server/domain/chat/presentation/dto/request/ReadQuizAndUserRequest.java
+++ src/main/java/com/space/server/domain/chat/presentation/dto/request/ReadQuizAndUserRequest.java
@@ -1,6 +1,6 @@
 package com.space.server.domain.chat.presentation.dto.request;
 
 public record ReadQuizAndUserRequest(
-    Long quizId,
-    Long userId
-) {}
+    Long userId,
+    Long quizId
+    ) {}
