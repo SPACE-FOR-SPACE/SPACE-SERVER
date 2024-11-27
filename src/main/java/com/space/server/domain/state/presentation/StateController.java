f7d82869c650d5921549e6d2bbffbd9422767ada
GSB0203 <gimseungbin0203@gmail.com>
2024-11-28 08:40:49 +0900

    feat(State): StateController
    User별 State 조회를 구현하였습니다.

--- src/main/java/com/space/server/domain/state/presentation/StateController.java
+++ src/main/java/com/space/server/domain/state/presentation/StateController.java
@@ -1,32 +1,40 @@
 package com.space.server.domain.state.presentation;
 
+import com.space.server.domain.state.presentation.dto.response.CheckStateResponse;
 import com.space.server.domain.state.presentation.dto.response.StateResponse;
 import com.space.server.domain.state.service.CommandStateService;
 import com.space.server.domain.state.service.QueryStateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/states")
 public class StateController {
 
     private final CommandStateService commandStateService;
     private final QueryStateService queryStateService;
 
-
-    @GetMapping("/states/{state-id}")
+    @GetMapping("/{state-id}")
     public StateResponse readOne(
             @PathVariable("state-id") Long stateId
     ) {
         return StateResponse.from(queryStateService.readOne(stateId));
     }
 
-    @DeleteMapping("/states/{state-id}")
+    @DeleteMapping("/{state-id}")
     public void deleteState(
             @PathVariable("state-id") Long stateId
     ) {
         commandStateService.delete(stateId);
     }
 
-
+    @GetMapping("/checkStates/{user-id}")
+    public List<CheckStateResponse> findAllByUserId(@PathVariable(name = "user-id") Long userId) {
+        return queryStateService.findAllByUserId(userId).stream()
+            .map(CheckStateResponse::from)
+            .toList();
+    }
 }
