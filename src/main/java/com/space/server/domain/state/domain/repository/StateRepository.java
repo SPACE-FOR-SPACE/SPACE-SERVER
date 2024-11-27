efe0d6dbcc61309579ce3eb9329e68f0b0728ebc
GSB0203 <gimseungbin0203@gmail.com>
2024-11-28 08:58:48 +0900

    refactor(State): StateRepository
    User Chapter별 State 조회로 수정하였습니다.

--- src/main/java/com/space/server/domain/state/domain/repository/StateRepository.java
+++ src/main/java/com/space/server/domain/state/domain/repository/StateRepository.java
@@ -1,9 +1,12 @@
 package com.space.server.domain.state.domain.repository;
 
+import com.space.server.domain.chapter.domain.Chapter;
 import com.space.server.domain.quiz.domain.Quiz;
 import com.space.server.domain.state.domain.State;
 import com.space.server.domain.user.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +16,10 @@ import java.util.Optional;
 public interface StateRepository extends JpaRepository<State, Long> {
     Optional<State> findByQuizAndUser(Quiz quiz, Users user);
 
-    List<State> findByUser(Users user);
+    List<State> findByUser(Users users);
+
+    @Query("SELECT s FROM State s " + "JOIN s.quiz q " + "WHERE q.chapter = :chapter AND s.user = :user")
+    List<State> findByChapterAndUser(@Param("chapter") Chapter chapter, @Param("user") Users user);
 
     List<State> findAllByUser(Users user);
 }
